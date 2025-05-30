6c39a7eaeca4d48dba8d019744065a9300bd6d38
D'Alessandro Arianna <Ar.DAlessandro@almaviva.it>
2025-05-30 14:42:05 +0200

    start project

---
+++ spring-6-di/src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+    @Autowired
+    private GreetingService greetingService;
+
+    //Costruttore
+    public void SetteriInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public GreetingService getGreetingService() {
+        return greetingService;
+    }
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    //Metodo saluto
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ spring-6-di/src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController propertyInjectedController;
+    @BeforeEach
+    void setUp() {
+
+        propertyInjectedController  = new PropertyInjectedController();
+        propertyInjectedController.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void sayHello() {
+
+        System.out.println("test property injected controller: " + propertyInjectedController.sayHello());
+    }
+
+
+}
